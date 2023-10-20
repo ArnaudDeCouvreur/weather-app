5f6b88587ca55f83f469870655d8bcbf8cef21c6
ArnaudDeCouvreur <decouvreur.arnaud@gmail.com>
2023-10-20 13:51:28 +0200

    static frontend

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    
+    speed: 1000,
+    effect: 'fade',
+    watchSlidesVisibility: true,
+loadPrevNext: true,
+
+    autoplay: {
+        delay: 2000,
+      },
+  });
